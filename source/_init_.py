dbdd857c70ce36d46ddf716f740accdfa43330fc
yTrashzin <91160692+Trasher151@users.noreply.github.com>
2022-06-02 19:57:08 -0300

    Update _init_.py

--- source/_init_.py
+++ source/_init_.py
@@ -20,5 +20,5 @@ except:
 	else: print(str(C)+'['+str(B)+'Opção Inválida'+str(C)+']');system('rm -rf config');exit()
 	config.close();restart()
 config=config_1
-if config == '1': exec(get('https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/src/menuTerminal.py').text)
+if config == '1': exec(get('https://raw.githubusercontent.com/Trasher151/Kiny-Tr/main/source/src/menuTerminal.py').text)
 elif config == '2': exec(get('https://raw.githubusercontent.com/Kiny-Kiny/Kiny-Painel/main/source/src/menuTouch.py').text)
